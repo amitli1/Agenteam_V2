244d571cffee3132beb86f383ce7a1da2281972a
amitli <amitli@rafael.co.il>
2026-08-17 10:34:44 +0300

    support usb audio jetson

---
+++ dockers/ground/jetson/download_openwakeword.py
@@ -0,0 +1,8 @@
+import openwakeword
+import os
+import pathlib
+
+if __name__ == "__main__":
+
+    # download to: '/path/to/venv/lib/python3.x/site-packages/openwakeword/resources/models/'
+    openwakeword.utils.download_models()
---
+++ dockers/ground/jetson/readme.md
@@ -0,0 +1,6 @@
+# openwakeword:
+```
+    download open wake word models to:
+    /mnt/nvme/python_venvs/agentVenv/lib/python3.10/site-packages/openwakeword/resources/models/
+    openwakeword.utils.download_models()
+```
